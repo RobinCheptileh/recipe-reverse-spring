e4c677d89510fc89c565d722e199861b8c66cbb3
Robin Cheptileh <robincheptileh@gmail.com>
2019-05-16 13:34:11 +0300

    :tada: Initial commit

---
+++ src/main/java/com/cognition/recipereverse/RecipeReverseApplication.java
@@ -0,0 +1,13 @@
+package com.cognition.recipereverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeReverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeReverseApplication.class, args);
+    }
+
+}
