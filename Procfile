52b7fb4b56c69f801ccf3f78ee7613f30562a03d
Robin Cheptileh <robincheptileh@gmail.com>
2019-05-21 19:37:32 +0300

    :wrench: update configs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar build/libs/recipe-reverse-1.0.jar --server.port=$PORT
+web: java -jar build/libs/recipe-reverse-1.0.jar
--- src/main/java/com/cognition/recipereverse/RecipeReverseApplication.java
+++ src/main/java/com/cognition/recipereverse/RecipeReverseApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecipeReverseApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RecipeReverseApplication.class, args);
     }
-
-
-
 }
